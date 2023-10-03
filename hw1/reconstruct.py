9536c397ca9766fb9b924e888534946e5ddb1d40
AlanHuang <alan0988469026@gmail.com>
2023-10-04 01:25:42 +0800

    my_icp v1 (slow)

--- hw1/reconstruct.py
+++ hw1/reconstruct.py
@@ -3,7 +3,8 @@ import open3d as o3d
 import copy
 import argparse
 import glob
-np.set_printoptions(suppress=True)
+# from tqdm.auto import tqdm
+# np.set_printoptions(suppress=True)
 
 width, height = 512, 512
 fov = 90
@@ -102,10 +103,89 @@ def local_icp_algorithm(source_down, target_down, trans_init, threshold):
     return result
 
 
-def my_local_icp_algorithm(source_down, target_down, trans_init, voxel_size):
-    # TODO: Write your own ICP function
-    raise NotImplementedError
+def my_local_icp_algorithm(source_down, target_down, trans_init, threshold, max_iter=30, relative_fitness=1e-3, relative_rmse=1e-3):
+    # ref slides: https://cs.gmu.edu/~kosecka/cs685/cs685-icp.pdf
+    pts_src = np.array(source_down.points)  # N x 3
+    pts_tgt = np.array(target_down.points)  # M x 3
+
+    # one-iter transformation
+    T = trans_init
+    R, t = T[:3, :3], T[:3, 3]
+    # accumulated transformation
+    T_accu = np.array(T)
+
+    prev_rmse = np.inf
+    prev_fitness = 0
+    for i in range(max_iter):
+        pts_src = (R @ pts_src.T).T + t  # N x 3
+
+        # nearest matching
+        tgt_ids = my_find_nearest_neighbors(pts_src, pts_tgt)
+        pts_dst = pts_tgt[tgt_ids]
+
+        # inlier selection
+        inlier_pts_src, inlier_pts_dst = my_inlier_selection(pts_src, pts_dst, threshold)
+
+        # transformation estimation
+        R, t = my_compute_transformation(inlier_pts_src, inlier_pts_dst)
+
+        # update
+        T = np.eye(4)
+        T[:3, :3] = R
+        T[:3, 3] = t
+        T_accu = T @ T_accu
+
+        # check convergence
+        inlier_rmse = my_compute_rmse(inlier_pts_src, inlier_pts_dst)
+        fitness = len(np.unique(tgt_ids)) / len(pts_src)
+        if (prev_rmse - inlier_rmse) < relative_rmse and (fitness - prev_fitness) < relative_fitness:
+            break
+        prev_rmse = inlier_rmse
+        prev_fitness = fitness
+
+    result = o3d.pipelines.registration.RegistrationResult()
+    result.transformation = T_accu
+    result.fitness = fitness
+    result.inlier_rmse = inlier_rmse
     return result
+
+
+def my_find_nearest_neighbors(src, tgt):
+    expanded_src = np.expand_dims(src, axis=1)  # N x 1 x 3
+    expanded_tgt = np.expand_dims(tgt, axis=0)  # 1 x M x 3
+    dist = np.linalg.norm(expanded_src - expanded_tgt, axis=2)  # N x M
+    tgt_ids = np.argmin(dist, axis=1)  # N
+    return tgt_ids
+
+
+def my_compute_rmse(src, dst):
+    rmse = np.mean(np.linalg.norm(src - dst, axis=1))
+    return rmse
+
+
+def my_inlier_selection(src, tgt, threshold):
+    dist = np.linalg.norm(src - tgt, axis=1)
+    inlier_mask = dist < threshold
+    inlier_src = src[inlier_mask]
+    inlier_tgt = tgt[inlier_mask]
+    return inlier_src, inlier_tgt
+
+
+def my_compute_transformation(src, tgt):
+    # centroid (center of mass)
+    centroid_src = np.mean(src, axis=0)  # 3
+    centroid_dst = np.mean(tgt, axis=0)  # 3
+    centered_src = src - centroid_src  # N x 3
+    centered_dst = tgt - centroid_dst  # N x 3
+
+    # SVD
+    W = centered_dst.T @ centered_src   # 3 x 3
+    U, _, Vt = np.linalg.svd(W)
+
+    # transform
+    R = U @ Vt
+    t = centroid_dst - R @ centroid_src
+    return R, t
 
 
 def reconstruct(args):
@@ -113,10 +193,11 @@ def reconstruct(args):
     data_root = args.data_root
     voxel_size = args.voxel_size
 
+    icp_algo = None
     if args.version == 'open3d':
-        trans = local_icp_algorithm
+        icp_algo = local_icp_algorithm
     elif args.version == 'my_icp':
-        trans = my_local_icp_algorithm
+        icp_algo = my_local_icp_algorithm
     else:
         raise NotImplementedError
 
@@ -126,6 +207,7 @@ def reconstruct(args):
     poses = []
     merged_pcd = o3d.geometry.PointCloud()
     prev_down, prev_fpfh = None, None
+    # for i in tqdm(range(1, seq_len+1)):
     for i in range(1, seq_len+1):
         rgb_img = o3d.io.read_image(f'{data_root}rgb/{i}.png')
         dep_img = o3d.io.read_image(f'{data_root}depth/{i}.png')
@@ -137,9 +219,8 @@ def reconstruct(args):
             reg_global = execute_global_registration(
                 now_down, prev_down, now_fpfh, prev_fpfh, voxel_size)
             # local registration
-            # FIXME: change to trans() later
-            reg_p2p = local_icp_algorithm(
-                now_down, prev_down, reg_global.transformation, threshold=voxel_size*0.4)
+            reg_p2p = icp_algo(
+                now_down, prev_down, reg_global.transformation, threshold=voxel_size)
 
             # transform from now to previous
             T_ij = reg_p2p.transformation
@@ -188,9 +269,6 @@ if __name__ == '__main__':
         args.data_root = "data_collection/second_floor/"
 
     # TODO: Output result point cloud and estimated camera pose
-    '''
-    Hint: Follow the steps on the spec
-    '''
     result_pcd, pred_poses = reconstruct(args)
 
     # TODO: Calculate and print L2 distance
@@ -200,9 +278,7 @@ if __name__ == '__main__':
     # ground truth translation
     gt_poses = np.load(f'{args.data_root}GT_pose.npy')
     gt_ts = gt_poses[:, :3]
-    gt_ts = gt_ts - gt_ts[0]
-    # FIXME: why gt translation is 10 times larger than the 3D scene
-    gt_ts = gt_ts * GT_T_SCALE
+    gt_ts = (gt_ts - gt_ts[0]) * GT_T_SCALE
     # predicted translation
     pred_ts = pred_poses[:, :3]
     # L2 distance
@@ -238,10 +314,17 @@ if __name__ == '__main__':
     pred_lineset.colors = o3d.utility.Vector3dVector(pred_colors)
 
     # Masking out the ceiling
-    ceiling_y_threshold = 0.4 * GT_T_SCALE
+
+    ceiling_y_threshold = 0.0 * GT_T_SCALE
     ceiling_mask = np.array(result_pcd.points)[:, 1] < ceiling_y_threshold
     cropped_pcd = result_pcd.select_by_index(np.where(ceiling_mask)[0])
-
     o3d.visualization.draw_geometries(
         [cropped_pcd, pred_lineset, gt_lineset],
     )
+
+    # result_down, _ = preprocess_point_cloud(result_pcd, voxel_size=0.005)
+    # ceiling_mask_down = np.array(result_down.points)[:, 1] < ceiling_y_threshold
+    # cropped_down = result_down.select_by_index(np.where(ceiling_mask_down)[0])
+    # o3d.visualization.draw_geometries(
+    #     [cropped_down, pred_lineset, gt_lineset],
+    # )
