fca0eda8c5b3551b1e6337395b78a64fbfe796b6
AlanHuang <alan0988469026@gmail.com>
2023-10-16 00:15:21 +0800

    format code

--- hw1/load.py
+++ hw1/load.py
@@ -35,9 +35,11 @@ cam_extr = []
 def transform_rgb_bgr(image):
     return image[:, :, [2, 1, 0]]
 
+
 def transform_depth(image):
     depth_img = (image / 10 * 255).astype(np.uint8)
     return depth_img
+
 
 def transform_semantic(semantic_obs):
     semantic_img = Image.new("P", (semantic_obs.shape[1], semantic_obs.shape[0]))
@@ -46,6 +48,7 @@ def transform_semantic(semantic_obs):
     semantic_img = semantic_img.convert("RGB")
     semantic_img = cv2.cvtColor(np.asarray(semantic_img), cv2.COLOR_RGB2BGR)
     return semantic_img
+
 
 def make_simple_cfg(settings):
     # simulator backend
@@ -68,7 +71,7 @@ def make_simple_cfg(settings):
     ]
     rgb_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
 
-    #depth snesor
+    # depth snesor
     depth_sensor_spec = habitat_sim.CameraSensorSpec()
     depth_sensor_spec.uuid = "depth_sensor"
     depth_sensor_spec.sensor_type = habitat_sim.SensorType.DEPTH
@@ -81,7 +84,7 @@ def make_simple_cfg(settings):
     ]
     depth_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
 
-    #semantic snesor
+    # semantic snesor
     semantic_sensor_spec = habitat_sim.CameraSensorSpec()
     semantic_sensor_spec.uuid = "semantic_sensor"
     semantic_sensor_spec.sensor_type = habitat_sim.SensorType.SEMANTIC
@@ -98,10 +101,11 @@ def make_simple_cfg(settings):
 
     return habitat_sim.Configuration(sim_cfg, [agent_cfg])
 
+
 def navigateAndSee(action="", data_root='data_collection/second_floor/'):
     global count
     observations = sim.step(action)
-    #print("action: ", action)
+    # print("action: ", action)
 
     cv2.imshow("RGB", transform_rgb_bgr(observations["color_sensor"]))
     cv2.imshow("depth", transform_depth(observations["depth_sensor"]))
@@ -110,19 +114,20 @@ def navigateAndSee(action="", data_root='data_collection/second_floor/'):
     sensor_state = agent_state.sensor_states['color_sensor']
     print("Frame:", count)
     print("camera pose: x y z rw rx ry rz")
-    print(sensor_state.position[0],sensor_state.position[1],sensor_state.position[2], sensor_state.rotation.w, sensor_state.rotation.x, sensor_state.rotation.y, sensor_state.rotation.z)
-    
+    print(sensor_state.position[0], sensor_state.position[1], sensor_state.position[2],
+          sensor_state.rotation.w, sensor_state.rotation.x, sensor_state.rotation.y, sensor_state.rotation.z)
+
     count += 1
     cv2.imwrite(data_root + f"rgb/{count}.png", transform_rgb_bgr(observations["color_sensor"]))
     cv2.imwrite(data_root + f"depth/{count}.png", transform_depth(observations["depth_sensor"]))
     cv2.imwrite(data_root + f"semantic/{count}.png", transform_semantic(observations["semantic_sensor"]))
-    
-    cam_extr.append([sensor_state.position[0], sensor_state.position[1], sensor_state.position[2], 
+
+    cam_extr.append([sensor_state.position[0], sensor_state.position[1], sensor_state.position[2],
                     sensor_state.rotation.w, sensor_state.rotation.x, sensor_state.rotation.y, sensor_state.rotation.z])
-    
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--floor', type=int, default=1)  
+parser.add_argument('-f', '--floor', type=int, default=1)
 args = parser.parse_args()
 
 cfg = make_simple_cfg(sim_settings)
@@ -145,10 +150,10 @@ action_names = list(cfg.agents[sim_settings["default_agent"]].action_space.keys(
 print("Discrete action space: ", action_names)
 
 
-FORWARD_KEY="w"
-LEFT_KEY="a"
-RIGHT_KEY="d"
-FINISH="f"
+FORWARD_KEY = "w"
+LEFT_KEY = "a"
+RIGHT_KEY = "d"
+FINISH = "f"
 print("#############################")
 print("use keyboard to control the agent")
 print(" w for go forward  ")
@@ -163,7 +168,7 @@ if args.floor == 1:
 elif args.floor == 2:
     data_root = "data_collection/second_floor/"
 
-if os.path.isdir(data_root): 
+if os.path.isdir(data_root):
     shutil.rmtree(data_root)  # WARNING: this line will delete whole directory with files
 
 for sub_dir in ['rgb/', 'depth/', 'semantic/']:
