af9239b68749c625f19ede1ea3a7651aaabca012
AlanHuang <alan0988469026@gmail.com>
2023-10-16 00:57:06 +0800

    fix bugs and add comments

--- hw1/bev.py
+++ hw1/bev.py
@@ -50,52 +50,68 @@ class Projection(object):
             Project the top view pixels to the front view pixels.
             :return: New pixels on perspective(front) view image
 
-            # Coordinate system
-            # Use right-hand coordinate system
-            # W - world coordinate system: (x, y, z) = (left, up, front)
-            # C - camera coordinate system: (x, y, z) = (right, down, front)
+            # Notation
+            # h - homogeneous coordinate
+            # W - in world coordinate system
+            # C - in camera coordinate system
+            # R_a_b = rotation matrix from b to a
+            # t_a_b = translation vector from b to a
+            # K - camera intrinsic matrix
+            # H - homography matrix
+
+            # Coordinate System Definition
+            # Use the right-hand coordinate system
+            # world coordinate system: (x, y, z) = (left, up, front)
+            # pixel coordinate system: (x, y, z) = (right, down, front)
+
             # Assumption
-            # R, t - known poses of bev view and front view
-            # K - known camera intrinsic matrix and it is the same for both bev view and front view
-            # n_bev_C - known the normal vector of the plane where points lies on in the bev view
-            # d_bev_C - known the distance from the origin to the plane in bev view
+            # R, t - known poses of the cameras
+            # K - known the camera intrinsic matrix and it is the same for both bev view and front view
+            # n_bev_C - known the normal vector of the plane which points lies on in the bev view
+            # d_bev_C - known the distance between the origin and the plane in bev view
         """
 
         x_bev = np.array(self.points).T  # (2, n)
         x_bev_h = np.vstack((x_bev, np.ones((1, x_bev.shape[1]))))  # (3, n)
 
+        # ----- camera extrinsic matrix -----
         t_bev_W = np.array([[0], [2.5], [0]])
         t_front_W = np.array([[0], [1], [0]])
-        # Note: watch out the direction of normal vector
-        n_bev_C = np.array([[0], [0], [1]])
-        d_bev_C = t_bev_W[1]  # scalar
 
-        # translation from the the front camera to the bev camera
-        t_front_bev = t_bev_W - t_front_W
-        # rotation from the front camera to the bev camera
         R_bev_front = xyz2R(theta, phi, gamma)
-        # rotation from the bev camera to the front camera
         R_front_bev = R_bev_front.T
+        t_front_bev = t_bev_W - t_front_W
 
-        # focal_length * tan(fov / 2) = width / 2
-        fx = self.width / 2 / np.tan(np.deg2rad(fov / 2))
-        fy = self.height / 2 / np.tan(np.deg2rad(fov / 2))
+        # ----- camera intrinsic matrix -----
+        # focal_length * tan(fov / 2) = image_size / 2
+        fx = - self.width / 2 / np.tan(np.deg2rad(fov / 2))
+        fy = - self.height / 2 / np.tan(np.deg2rad(fov / 2))
         cx = self.width / 2
         cy = self.height / 2
         K = np.array([
-            [-fx, 0, cx],
-            [0, -fy, cy],
-            [0, 0, 1]
+            [fx, 0, cx],
+            [0, fy, cy],
+            [0, 0, 1],
         ])
-        K_inv = np.linalg.inv(K)
+        K_inv = np.array([
+            [1 / fx, 0, - cx / fx],
+            [0, 1 / fy, - cy / fy],
+            [0, 0, 1],
+        ])
+        # K_inv = np.linalg.inv(K)
 
+        # ----- homography matrix -----
+        # NOTE: watch out the direction of normal vector.
+        n_bev_C = np.array([[0], [0], [1]])
+        d_bev_C = t_bev_W[1]  # (scalar)
         H = K @ (R_front_bev + t_front_bev * n_bev_C.T / d_bev_C) @ K_inv
 
         x_front_h = H @ x_bev_h
-        x_front_h = x_front_h / x_front_h[2, :]
-        x_front = x_front_h[:2, :]
+        x_front_h = x_front_h / x_front_h[2, :]  # (3, n)
 
+        x_front = x_front_h[:2, :]  # (2, n)
         new_pixels = x_front.T.astype(int)
+
         return new_pixels
 
     def show_image(self, new_pixels, img_name='projection.png', color=(0, 0, 255), alpha=0.4):
@@ -127,6 +143,7 @@ def click_event(event, x, y, flags, params):
         # cv2.putText(img, str(x) + ',' + str(y), (x+5, y+5), font, 0.5, (0, 0, 255), 1)
         cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
         cv2.imshow('image', img)
+        # cv2.imwrite('bev_points.png', img)
 
     # checking for right mouse clicks
     if event == cv2.EVENT_RBUTTONDOWN:
