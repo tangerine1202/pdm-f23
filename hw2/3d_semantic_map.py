ba593762cc78f54914b2a95d5b798ec42d6c60b1
AlanHuang <alan0988469026@gmail.com>
2023-10-20 03:47:03 +0800

    add custom voxel down

--- hw2/3d_semantic_map.py
+++ hw2/3d_semantic_map.py
@@ -1,9 +1,12 @@
 import argparse
 import glob
 import copy
+from collections import Counter
 import numpy as np
 import open3d as o3d
+
 from tqdm.auto import tqdm
+import time
 
 from hw1 import (
     depth_image_to_point_cloud,
@@ -72,7 +75,91 @@ def reconstruct(args):
 
 def custom_voxel_down(pcd, voxel_size):
     # TODO: implement your own voxel down
-    raise NotImplementedError
+    """
+    Voxel downsampling uses a regular voxel grid to create a uniformly downsampled point cloud from an input point cloud.
+
+    The algorithm operates in two steps:
+    1. Points are bucketed into voxels.
+    2. Each occupied voxel generates exactly one point by averaging all points inside.
+    ref: http://www.open3d.org/docs/release/tutorial/geometry/pointcloud.html#Voxel-downsampling
+    """
+    # t0 = time.time()
+    points = np.array(pcd.points)
+    colors = np.array(pcd.colors)
+
+    # FIXME: 0.03 - 40-60% / 0.01 - 5%
+    # unique_t0 = time.time()
+    unique_colors, color_ids = np.unique(colors, axis=0, return_inverse=True)
+    # unique_t = time.time() - unique_t0
+
+    # ignorble runtime
+    xbins = np.arange(np.min(points[:, 0]), np.max(points[:, 0]), voxel_size)
+    ybins = np.arange(np.min(points[:, 1]), np.max(points[:, 1]), voxel_size)
+    zbins = np.arange(np.min(points[:, 2]), np.max(points[:, 2]), voxel_size)
+    pts_xbin_idx = np.digitize(points[:, 0], xbins)
+    pts_ybin_idx = np.digitize(points[:, 1], ybins)
+    pts_zbin_idx = np.digitize(points[:, 2], zbins)
+
+    # FIXME: 0.03 - 30-50% / 0.01 - 90%
+    # loop_t0 = time.time()
+    # bool_op_t = 0
+    # where_t = 0
+    # color_t = 0
+    voxel_points = []
+    voxel_colors = []
+    for i in range(len(xbins)):
+        xbin_mask = (pts_xbin_idx == i)
+        if not np.any(xbin_mask):
+            continue
+        for j in range(len(ybins)):
+            ybin_mask = (pts_ybin_idx == j)
+            # bool_op_t0 = time.time()
+            xybin_mask = (xbin_mask & ybin_mask)
+            # bool_op_t += time.time() - bool_op_t0
+            if not np.any(xybin_mask):
+                continue
+            for k in range(len(zbins)):
+                zbin_mask = (pts_zbin_idx == k)
+
+                # FIXME: 0.03 - 10% / 0.01 - 15%
+                # bool_op_t0 = time.time()
+                bin_mask = xybin_mask & zbin_mask
+                # bool_op_t += time.time() - bool_op_t0
+                if not np.any(bin_mask):
+                    continue
+
+                # FIXME: 0.03 - 4~% / 0.01 - 2-3%
+                # where_t0 = time.time()
+                voxel_idx = np.nonzero(bin_mask)[0]
+                # where_t += time.time() - where_t0
+
+                # FIXME: 0.03 - 5~% / 0.01 - 1-2%
+                # take the most common color in the voxel
+                # color_t0 = time.time()
+                most_common_color_id = Counter(color_ids[voxel_idx]).most_common(1)[0][0]
+                voxel_color = unique_colors[most_common_color_id]
+                # color_t += time.time() - color_t0
+
+                # put the voxel point at the center of the voxel
+                voxel_point = np.array([xbins[i] + voxel_size / 2,
+                                        ybins[j] + voxel_size / 2,
+                                        zbins[k] + voxel_size / 2])
+
+                voxel_points.append(voxel_point)
+                voxel_colors.append(voxel_color)
+    # loop_t = time.time() - loop_t0
+    # total_t = time.time() - t0
+    # print(f'unique_t:   {unique_t:.4f} {unique_t / total_t:.4f}%')
+    # print(f'bool_op_t:  {bool_op_t:.4f} {bool_op_t / total_t:.4f}%')
+    # print(f'where_t:    {where_t:.4f} {where_t / total_t:.4f}%')
+    # print(f'color_t:    {color_t:.4f} {color_t / total_t:.4f}%')
+    # print(f'loop_t:     {loop_t:.4f} {loop_t / total_t:.4f}%')
+    # print(f'total_t:    {total_t:.4f} {total_t / total_t:.4f}%')
+
+    pcd_down = o3d.geometry.PointCloud()
+    pcd_down.points = o3d.utility.Vector3dVector(np.array(voxel_points))
+    pcd_down.colors = o3d.utility.Vector3dVector(np.array(voxel_colors))
+    return pcd_down
 
 
 def preprocess_point_cloud(pcd, voxel_size, method='my'):
